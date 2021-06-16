29c65881989f3ebc25b1ad97c87a9275b7c20b73
ghiyastfarisi <ghi.fai@gmail.com>
2021-06-16 20:08:49 +0700

    update infra thins

--- run-production.sh
+++ run-production.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-docker run -it --rm --name democi4 \
--p 8081:80 \
+docker run -d --name democi4 \
+--network=bridge \
+-p 80:80 \
+-p 443:443 \
 -v $(pwd)/public/files:/var/www/html/public/files \
 -v $(pwd)/.env:/var/www/html/.env \
 democi4:v1
