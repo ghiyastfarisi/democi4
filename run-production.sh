cd40009c1e48b569ea17bed5f7da76dc6667c5aa
ghiyastfarisi <ghi.fai@gmail.com>
2021-07-03 20:19:51 +0700

    change prod port to 8080

--- run-production.sh
+++ run-production.sh
@@ -4,7 +4,7 @@ docker rm democi4
 docker run -d --name democi4 \
     --restart=always \
     --network=bridge \
-    -p 80:80 \
+    -p 8080:80 \
     -v $(pwd)/public/files:/var/www/html/public/files \
     -v $(pwd)/.env:/var/www/html/.env \
     democi4:v1
