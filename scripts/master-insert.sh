f862576a84fe983eb1bdeb38fcb47bc093caba04
ghiyastfarisi <ghi.fai@gmail.com>
2021-07-06 11:01:44 +0700

    change script

--- scripts/master-insert.sh
+++ scripts/master-insert.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "executing import..."
-mysql -h 192.168.99.99 -u root -p1XAmpleXample1 db_upi_dacen < master.sql
+mysql --protocol=tcp -u root -p1XAmpleXample1 db_upi_dacen < master.sql
 echo "executing done"
 sleep 3
 echo "checking tables..."
-mysql -h 192.168.99.99 -u root -p1XAmpleXample1 db_upi_dacen -e "show tables"
+mysql --protocol=tcp -u root -p1XAmpleXample1 db_upi_dacen -e "show tables"
